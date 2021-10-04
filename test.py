e2583a1af89f3937ebda74c60a5433aa17476397
Avery <averykhoo@gmail.com>
2021-10-04 17:21:22 +0800

    test write and read

--- test.py
+++ test.py
@@ -5,10 +5,17 @@ import ldif_parser
 
 def main():
     with open('sample.ldif') as f:
-        ldif = ldif_parser.load(f)
-        for entry in ldif:
+        entries = ldif_parser.load(f)
+        for entry in entries:
             print(entry)
             pprint(entry.attributes)
+
+    with open('formatted.ldif', 'w') as f:
+        ldif_parser.dump(f, entries, line_width=20)
+
+    with open('formatted.ldif') as f:
+        assert entries == ldif_parser.load(f)
+
 
 
 if __name__ == '__main__':
