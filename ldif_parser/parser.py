e7446fe63cc19d8ca82ca6e218653c7edd22edb2
Avery <averykhoo@gmail.com>
2021-10-04 17:20:47 +0800

    write ldif files

--- ldif_parser/parser.py
+++ ldif_parser/parser.py
@@ -1,6 +1,7 @@
 import base64
 from typing import Any
 from typing import Generator
+from typing import Iterable
 from typing import List
 from typing import Optional
 from typing import TextIO
@@ -22,6 +23,7 @@ def lazy_load(file_obj: TextIO,
     """
     _buffer = []
 
+    # helper function to unwrap lines when reading from file
     def read_unfolded_line() -> Optional[str]:
         nonlocal _buffer
         nonlocal file_obj
@@ -35,11 +37,8 @@ def lazy_load(file_obj: TextIO,
                     tmp = ''.join(_buffer)
                     _buffer = []
                     return tmp
-                return None
-
-            # skip comment line
-            if next_line.startswith('#'):
-                continue
+                else:
+                    return None
 
             # remove trailing newline
             next_line = next_line.rstrip('\r\n')
@@ -59,6 +58,7 @@ def lazy_load(file_obj: TextIO,
             _buffer = [next_line]
             return tmp
 
+    # read entries
     current_entry = Entry()
     while True:
         line = read_unfolded_line()
@@ -67,13 +67,17 @@ def lazy_load(file_obj: TextIO,
         if line is None:
             if current_entry.attributes:
                 yield current_entry
-                return
+            return
 
         # end of entry
         if not line:
             if current_entry.attributes:
                 yield current_entry
                 current_entry = Entry()
+            continue
+
+        # comment line
+        if line.startswith('#'):
             continue
 
         # parse line
@@ -107,9 +111,69 @@ def load(file_obj: TextIO,
 
 
 def dump(file_obj: TextIO,
-         ldap_entries: List[Entry],
+         ldap_entries: Iterable[Entry],
          *,
          version: Optional[int] = 1,
          line_width: Optional[int] = 76,
          ) -> int:
-    pass  # todo
+    """
+    write Entry instances to a file
+
+    :param file_obj: open('sample.ldif', 'w', encoding='ascii')
+    :param ldap_entries: list of Entry instances
+    :param version: version number to write, or None to skip writing
+    :param line_width: the RFC says 76, but most systems will parse whatever you put in
+    :return: number of objects written
+    """
+
+    # sanity check the line width
+    if line_width is not None:
+        if not isinstance(line_width, int):
+            raise TypeError(line_width)
+        if line_width < 2:
+            raise ValueError(line_width)
+
+    # helper function to wrap lines when writing to file
+    def write_folded_line(line):
+        if line_width is None:
+            file_obj.write(f'{line}\n')
+        else:
+            file_obj.write(f'{line[:line_width]}\n')
+            line = line[line_width:]
+            while line:
+                file_obj.write(f' {line[:line_width - 1]}\n')
+                line = line[line_width - 1:]
+
+    # write version
+    if version is not None:
+        if not isinstance(version, int):
+            raise TypeError(version)
+        write_folded_line(f'version: {version}')
+        write_folded_line('')
+
+    # write all entries
+    n_objs_written = 0
+    for entry in ldap_entries:
+        for attribute_key, attribute_value in entry.attributes:
+
+            # write string
+            if isinstance(attribute_value, str):
+                write_folded_line(f'{attribute_key}: {attribute_value}')
+
+            # write bytes
+            elif isinstance(attribute_value, bytes):
+                write_folded_line(f'{attribute_key}:: {base64.b64encode(attribute_value).decode("ascii")}')
+
+            # write URL
+            elif isinstance(attribute_value, URL):
+                write_folded_line(f'{attribute_key}:< {attribute_value.text}')
+
+            else:
+                raise TypeError((attribute_key, attribute_value))
+
+        # write at least one empty line
+        write_folded_line('')
+        n_objs_written += 1
+
+    # return the number of objects written to the file
+    return n_objs_written
